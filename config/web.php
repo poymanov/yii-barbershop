2671d5c5c98ac467bf56f37a8ecc5396f19d925d
Nikolay <n.poymanov@gmail.com>
2016-05-19 23:16:23 +0300

    [new] init project
    * Инициализация проекта
    * Модификация .gitignore
    * Реализован функционал объедения локальных настроек и общедоступных

---
+++ config/web.php
@@ -0,0 +1,72 @@
+<?php
+
+function merge_configs($base, $customized)
+{
+    $baseConfig = require($base);
+    if (is_file($customized))
+        return yii\helpers\ArrayHelper::merge($baseConfig, require($customized));
+    return $baseConfig;
+}
+
+$params = merge_configs(__DIR__ . '/params.php', __DIR__ . '/params.local.php');
+$db = merge_configs(__DIR__ . '/db.php', __DIR__ . '/db.local.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => $params['cookieValidationKey'],
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ]
+        ]
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
